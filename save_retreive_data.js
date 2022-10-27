396ed1a9bd114e19da54544b37a42782fc5e5f45
For Fun <dnce17@Dannys-MacBook-Air.local>
2022-10-27 16:19:21 -0400

    delete function made

--- save_retreive_data.js
+++ save_retreive_data.js
@@ -1,12 +1,14 @@
 let saveBtn = document.querySelector(".save");
 let retreiveBtn = document.querySelector(".retreive");
 let deleteBtn = document.querySelector(".delete");
+let deleteAllBtn = document.querySelector(".delete-all");
 let info = document.querySelector(".display-info")
 let saved = document.getElementById("saved");
 
 saveBtn.addEventListener("click", save);
 retreiveBtn.addEventListener("click", retreive);
 deleteBtn.addEventListener("click", deleteItem)
+deleteAllBtn.addEventListener("click", deleteAll)
 
 function checkDup(nameToBeSaved, option) {
     let alreadyExist = [];
@@ -20,12 +22,12 @@ function checkDup(nameToBeSaved, option) {
     } else {
         saved.add(option);
         alert("Save Successful!");
-        // let saveAs = document.getElementById("save-as")
-        // saveAs.value = "";
+        let saveAs = document.getElementById("save-as")
+        saveAs.value = "";
     }
 }
 
-function save(nameToBeSaved) {
+function save() {
     let name = document.getElementById("name").value;
     let age = document.getElementById("age").value;
     let saveAs = document.getElementById("save-as").value; // this is basically what you name your saved file
@@ -42,32 +44,13 @@ function save(nameToBeSaved) {
     option.text = saveAs;
 
     checkDup(option.value, option)
-
-    // // check if duplicated in saved, then add if no duplicate
-    // let alreadyExist = [];
-    // for (i = 0; i < saved.length; i++) {
-    //     let maybeDup = saved[i].value;
-    //     alreadyExist.push(maybeDup);
-    // }
-
-    // if (alreadyExist.includes(saveAs)) {
-    //     alert("Name already taken. File will be overwritten.")
-    // } else {
-    //     saved.add(option);
-    //     alert("Save Successful!")
-    // }
 }
 
 function retreive() {
     let key = saved.value;
     let records = window.localStorage.getItem(key);
 
-    // show info in page
-    let text = document.createTextNode(records);
-    info.appendChild(text);
-
-    // show in console
-    console.log(JSON.parse(records).name);
+    // Load the saved data
     let name = document.getElementById("name");
     let age = document.getElementById("age");
     name.value = JSON.parse(records).name;
@@ -77,16 +60,52 @@ function retreive() {
 function deleteItem() {
     let key = saved.value;
     localStorage.removeItem(key);
-    console.log("item removed");
+
+    // remove the deleted item from saved input box 
+    let alreadyExist = [];
+    for (i = 0; i < saved.length; i++) {
+        let savedName = saved[i].value;
+        alreadyExist.push(savedName);
+    }
+
+    if (alreadyExist.includes(key)) {
+        for (i = 0; i < saved.length; i++) {
+            let savedName = saved[i].value
+            if (key == savedName) {
+                // console.log(saved[i], saved[i].value, key, i);
+                saved.remove(i);
+                alert("File has been deleted")
+            } 
+        }
+    }
 }
 
+function deleteAll() {
+    window.localStorage.clear();
+
+    for (i = saved.length - 1; i >= 0; i--) {
+        console.log(saved[i])
+        saved.remove(i);
+    }
+}
+
+// Load key names from local storage into saved input box
 const keys = Object.keys(localStorage)
 for (let key of keys) {
-    // console.log(key);
-
     let option = document.createElement("option");
     option.text = key;
     saved.add(option);
 }
 
-// for later
+
+// Potential use for future 
+// let text = document.createTextNode(records);
+// info.appendChild(text);
+
+// Resouces used
+// https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_select_remove
+// https://stackoverflow.com/questions/41112624/remove-select-option-with-specific-value
+// https://stackoverflow.com/questions/3364493/how-do-i-clear-all-options-in-a-dropdown-box
+
+// Of Note
+// remove() rearranges the index, so when you remove all options from select, you might run into issues 
